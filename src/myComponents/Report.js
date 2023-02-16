ac3c1c07ecc42672ee732f7048bec68e9f5283fa
ashishp911 <ashish.patil35@gmail.com>
2023-02-16 15:01:41 -0500

    creating report.js

---
+++ src/myComponents/Report.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Report = () => {
+  return (
+    <div>
+      <article className="widget">
+        
+      </article>
+    </div>
+  )
+}
+
+export default Report
